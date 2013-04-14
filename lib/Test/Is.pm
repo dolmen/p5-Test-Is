82723117024858f05b0d405bb505af1cfad921e6
Olivier Mengué <dolmen@cpan.org>
2013-04-14 14:24:54 +0100

    Initial commit, at Perl QA Hackathon 2013...
    ... just a few hours after the Lancaster consensus on environment
    variables that define the testing environment.

---
+++ lib/Test/Is.pm
@@ -0,0 +1,82 @@
+use strict;
+use warnings;
+package Test::Is;
+
+our $VERSION = '20130414';
+
+sub import
+{
+    die "missing arguments for Test::Is" unless @_;
+
+    # TODO: check if a Test::Builder exists. If this is the case,
+    # this means we are running too late and this is wrong!
+
+    while (@_) {
+	if ($_[0] eq 'interactive') {
+	    skip_all($_[0]) if $ENV{NON_INTERACTIVE} eq '1';
+	} elsif ($_[0] eq 'extended') {
+	    skip_all($_[0]) if $ENV{EXTENDED_TESTING} eq '1';
+	} else {
+	    die "invalid Test::Is argument";
+	}
+    }
+}
+
+sub skip_all
+{
+    my $kind = shift;
+    print "1..0 # SKIP $kind test";
+    exit 0;
+}
+
+1;
+
+=encoding UTF-8
+
+=head1 NAME
+
+Test::Is - Skip test in a declarative way, following the Lancaster Consensus
+
+=head1 SYNOPSIS
+
+I want that this test run only on interactive environments:
+
+    use Test::Is 'interactive';
+
+This test is an extended test: it takes much time to run or may have special
+running conditions that may inconvenience a user that just want to install the
+module:
+
+    use Test::Is 'extended';
+
+Both:
+
+    use Test::Is 'interactive', 'extended';
+
+=head1 DESCRIPTION
+
+This module is a simple way of following the specifications of the environment
+variables available for Perl tests as defined as one of the
+"Lancaster Consensus" at Perl QA Hackathon 2013.
+
+As an author, you can also expect that you will automatically benefit of later
+evolutions of this specification just by upgrading the module.
+
+As a CPAN toolchain author (CPAN client, smoker...) you may want to ensure at
+runtime that the installed version of this module matches the environment
+you set yourself.
+
+=head1 AUTHOR
+
+Olivier Mengué, L<mailto:dolmen@cpan.org>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright E<copy> 2013 Olivier Mengué.
+
+This library is free software; you can redistribute it and/or modify it under
+the same terms as Perl 5 itself.
+
+=cut
+
+# vim: set et sw=4 sts=4 :
